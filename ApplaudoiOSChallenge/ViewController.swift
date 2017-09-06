b62a9e0b38818ff9f53141e244b1fcda709dd554
Isidro Adan Garcia Solorio <igarcia@ia.com.mx>
2017-09-05 21:34:47 -0500

    Xcode project created

---
+++ ApplaudoiOSChallenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ApplaudoiOSChallenge
+//
+//  Created by Adan Garcia on 05/09/17.
+//  Copyright © 2017 Adan Garcia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
